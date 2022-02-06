51dbda5ecd1f1a8f980fdf4c2dc4261d1c993cef
buzzbean11 <98566336+buzzbean11@users.noreply.github.com>
2022-02-06 18:24:37 +0900

    Update example.py

--- Daedo/example.py
+++ Daedo/example.py
@@ -8,4 +8,11 @@ print(a % b)
 print(a ** b)
 print(a // b)
 
-# sdfdsf
+print("Hello World")
+print("Mary's cosmetic")
+print('신씨가 소리질렀다. "도둑이야".')
+print("c:\Windows")
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+print("naver", "kakao", "sk", "samsung", sep=";")
+print("naver", "kakao", "sk", "samsung", sep="/")
+print("first", end="");print("second")
