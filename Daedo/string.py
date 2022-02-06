ed3222de3ae0d3087e5bbbc112d9f1ba0efa7829
buzzbean11 <98566336+buzzbean11@users.noreply.github.com>
2022-02-06 17:49:51 +0900

    Create string.py

---
+++ Daedo/string.py
@@ -0,0 +1,17 @@
+head = "python "
+tail = "is fun!"
+print(head + tail)
+head = "python "
+print(head * 2)
+print("=" * 20)
+print("python is fun")
+print("=" * 20)
+a = "life is too shot, you need python"
+print(len(a))
+print(a[3])
+b = a[0] + a[1] + a[2] + a[3] + a[4]
+print(b)
+c = a[0:5]
+print(c)
+print (a[12:17])
+print (a[19:])
