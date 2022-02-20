5ccee3f2e505b0a49985b780642336fc957560c5
buzzbean11 <98566336+buzzbean11@users.noreply.github.com>
2022-02-20 17:43:53 +0900

    Create bool.py

---
+++ Daedo/bool.py
@@ -0,0 +1,21 @@
+# 불 자료형
+a = True
+b = False
+print(type(a))
+print(type(b))
+print(1 == 1)
+print(2 > 1)
+print(2 < 1)
+
+# 자료형의 참과 거짓
+c = [1, 2, 3, 4]
+while c:
+        print(c.pop())
+
+print(bool('python'))
+print(bool(''))
+print(bool([1,2,3]))
+print(bool([]))
+print(bool(0))
+print(bool(3))
+
